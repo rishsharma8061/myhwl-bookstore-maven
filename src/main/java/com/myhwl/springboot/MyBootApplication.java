0329d5bd59d7547425d54ab210e8baed6c654591
Rishi Sharma <rishi.sharma8061@gmail.com>
2022-07-11 15:00:46 +0530

    one main class in boot application

--- src/main/java/com/myhwl/springboot/MyBootApplication.java
+++ src/main/java/com/myhwl/springboot/MyBootApplication.java
@@ -5,11 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MyBootApplication {
-public static void main(String[] args) {
-System.out.println("Starting Boot Application");
-System.out.println("Update -1 Srinivas");
-  System.out.println("Update -2 by Srinivas");
-
-SpringApplication.run(MyBootApplication.class, args);
-}
+	public static void main(String[] args) {
+		System.out.println("Starting Boot Application");
+		System.out.println("Update -1 Rishi Sharma ");
+		System.out.println("Update -2 by Rishi Sharma");
+		SpringApplication.run(MyBootApplication.class, args);
+	}
 }
